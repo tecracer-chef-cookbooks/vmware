102b096963e95a61dafc33a678107c4a53882661
Patrick Schaumburg <pschaumburg@tecracer.de>
2020-05-04 12:22:24 +0200

    fix

--- recipes/tools.rb
+++ recipes/tools.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2020, tecRacer Opensource, Apache-2.0.
 
-if node['virtualization']['system'] == 'vmware'
+if node['virtualization']['system'] == 'vmware' || ENV['TEST_KITCHEN']
 
   execute 'add_vmware_repo_key' do
     command 'wget http://packages.vmware.com/tools/VMWARE-PACKAGING-GPG-KEY.pub -q -O- | apt-key add -'
@@ -15,14 +15,14 @@ if node['virtualization']['system'] == 'vmware'
     uri "http://packages.vmware.com/tools/esx/#{node['vmware']['esx']['tools']['version']}/ubuntu"
     components %w(main restricted)
     action :add
-    notifies :run, 'resource[add_vmware_repo_key]', :immediately
+    notifies :run, 'execute[add_vmware_repo_key]', :immediately
   end
 
   package 'vmware-tools-esx-nox' do
     action :install
   end
 
-  package "vmware-tools-esx-kmods-#{node.kernel.release}" do
+  package "vmware-tools-esx-kmods-#{node['kernel']['release']}" do
     ignore_failure true # Vmware can be slow to release package for new updated kernel
     action :install
   end
